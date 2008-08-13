71c9ad2252b0704e11f792a0a4a7a3b4f07081a9
Ali Shah <caffein@gmail.com>
2008-08-13 18:36:52 +0000

    Fixed licensing issue. Qlix is now GPL 2 only.
    git-svn-id: svn://svn.berlios.de/qlix/trunk@100 97db03ad-8736-0410-9762-8805f5232b6b

--- widgets/CommandCodes.h
+++ widgets/CommandCodes.h
@@ -1,15 +1,17 @@
 /*
  *   Copyright (C) 2008 Ali Shah <caffein@gmail.com>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ *   This file is part of the Qlix project on http://berlios.de
+ *
+ *   This file may be used under the terms of the GNU General Public
+ *   License version 2.0 as published by the Free Software Foundation
+ *   and appearing in the file COPYING included in the packaging of
+ *   this file.  
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ *   GNU General Public License version 2.0 for more details.
  *
  *   You should have received a copy of the GNU General Public License along
  *   with this program; if not, write to the Free Software Foundation, Inc.,
